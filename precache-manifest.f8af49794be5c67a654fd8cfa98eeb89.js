8959ad2846c22001f1eb601b5f59978ac0a8a347
Maneet Goyal <maneetgoyal14@gmail.com>
2019-11-11 00:18:11 +0530

    Updates

---
+++ precache-manifest.f8af49794be5c67a654fd8cfa98eeb89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c23c442b770dada3834c9010087863",
+    "url": "/date-formatter-intl-js/index.html"
+  },
+  {
+    "revision": "01461755efcb32ec7d67",
+    "url": "/date-formatter-intl-js/static/css/2.0f569cc2.chunk.css"
+  },
+  {
+    "revision": "8a610275d3af996828c7",
+    "url": "/date-formatter-intl-js/static/css/main.b0161697.chunk.css"
+  },
+  {
+    "revision": "01461755efcb32ec7d67",
+    "url": "/date-formatter-intl-js/static/js/2.be4a2830.chunk.js"
+  },
+  {
+    "revision": "8a610275d3af996828c7",
+    "url": "/date-formatter-intl-js/static/js/main.6e8b4fc9.chunk.js"
+  },
+  {
+    "revision": "fcd82f6489b7441e7d22",
+    "url": "/date-formatter-intl-js/static/js/runtime-main.aed3af8c.js"
+  }
+]);
